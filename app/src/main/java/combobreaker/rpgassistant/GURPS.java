1ff50289010bf5ff78d6cffab060ebb411cb1051
matthewwyndham <matthewwyndham@gmail.com>
2016-06-11 17:11:01 -0600

    added buttons. Still need to be put in individual relative views

--- app/src/main/java/combobreaker/rpgassistant/GURPS.java
+++ app/src/main/java/combobreaker/rpgassistant/GURPS.java
@@ -15,14 +15,5 @@ public class GURPS extends AppCompatActivity {
         setContentView(R.layout.activity_gurps);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
