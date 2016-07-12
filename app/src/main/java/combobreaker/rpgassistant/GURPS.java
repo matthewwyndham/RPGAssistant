0e62c6a5ff51c5972b97d6ac477ea7bea7ee768a
Jonathan <jester1570@gmail.com>
2016-07-11 19:12:51 -0600

    All buttons work for stats and current values.

--- app/src/main/java/combobreaker/rpgassistant/GURPS.java
+++ app/src/main/java/combobreaker/rpgassistant/GURPS.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class GURPS extends AppCompatActivity {
     // this is actually the character sheet... my bad, wrong name...
     int stcounter;
@@ -19,6 +21,8 @@ public class GURPS extends AppCompatActivity {
     int pecounter;
     int htcounter;
     int fpcounter;
+    int chpcounter;
+    int cfpcounter;
     SharedPreferences saved;
 
     @Override
@@ -60,6 +64,14 @@ public class GURPS extends AppCompatActivity {
         saved = getSharedPreferences("fpcounter", 0);
         fpcounter = saved.getInt("fpcounter", 0);
 
+        // chpcounter
+        saved = getSharedPreferences("hfpcounter", 0);
+        fpcounter = saved.getInt("chpcounter", 0);
+
+        // cfpcounter
+        saved = getSharedPreferences("cfpcounter", 0);
+        fpcounter = saved.getInt("cfpcounter", 0);
+
         update();
     }
 
@@ -74,30 +86,53 @@ public class GURPS extends AppCompatActivity {
 
         // dxcounter
         TextView c = (TextView) findViewById(R.id.dxview);
-        b.setText(dxcounter + "");
+        c.setText(dxcounter + "");
 
         // wicounter
         TextView d = (TextView) findViewById(R.id.willview);
-        b.setText(wicounter + "");
+        d.setText(wicounter + "");
 
         // iqcounter
         TextView e = (TextView) findViewById(R.id.iqview);
-        b.setText(iqcounter + "");
+        e.setText(iqcounter + "");
 
         // pecounter
         TextView f = (TextView) findViewById(R.id.perview);
-        b.setText(pecounter + "");
+        f.setText(pecounter + "");
 
         // htcounter
         TextView g = (TextView) findViewById(R.id.htview);
-        b.setText(htcounter + "");
+        g.setText(htcounter + "");
 
         // fpcounter
         TextView h = (TextView) findViewById(R.id.fpview);
-        b.setText(fpcounter + "");
+        h.setText(fpcounter + "");
+
+        //chpcounter
+        TextView i = (TextView) findViewById(R.id.chpview);
+        i.setText(chpcounter + "");
+
+        // cfpcounter
+        TextView j = (TextView) findViewById(R.id.cfpview);
+        j.setText(cfpcounter + "");
     }
 
     // st controls
+    public void plusst(View v) {
+        stcounter++;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("stcounter", stcounter);
+        editor.apply();
+        update();
+    }
+
+    public void minusst(View v) {
+        stcounter--;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("stcounter", stcounter);
+        editor.apply();
+        update();
+    }
 
     // hp controls
     public void plushp(View v) {
@@ -117,15 +152,139 @@ public class GURPS extends AppCompatActivity {
     }
 
     // dx controls
+    public void plusdx(View v) {
+        dxcounter++;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("dxcounter", dxcounter);
+        editor.apply();
+        update();
+    }
+
+    public void minusdx(View v) {
+        dxcounter--;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("dxcounter", dxcounter);
+        editor.apply();
+        update();
+    }
 
     // wi controls
+    public void pluswi(View v) {
+        wicounter++;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("wicounter", wicounter);
+        editor.apply();
+        update();
+    }
+
+    public void minuswi(View v) {
+        wicounter--;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("wicounter", wicounter);
+        editor.apply();
+        update();
+    }
 
     // iq controls
+    public void plusiq(View v) {
+        iqcounter++;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("iqcounter", iqcounter);
+        editor.apply();
+        update();
+    }
+
+    public void minusiq(View v) {
+        iqcounter--;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("iqcounter", iqcounter);
+        editor.apply();
+        update();
+    }
 
     // pe controls
+    public void pluspe(View v) {
+        pecounter++;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("ecounter", pecounter);
+        editor.apply();
+        update();
+    }
+
+    public void minuspe(View v) {
+        pecounter--;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("pecounter", pecounter);
+        editor.apply();
+        update();
+    }
 
     // ht controls
+    public void plusht(View v) {
+        htcounter++;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("htcounter", htcounter);
+        editor.apply();
+        update();
+    }
+
+    public void minusht(View v) {
+        htcounter--;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("htcounter", htcounter);
+        editor.apply();
+        update();
+    }
 
     // fp controls
+    public void plusfp(View v) {
+        fpcounter++;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("fpcounter", fpcounter);
+        editor.apply();
+        update();
+    }
+
+    public void minusfp(View v) {
+        fpcounter--;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("fpcounter", fpcounter);
+        editor.apply();
+        update();
+    }
+
+    // Current HP controls
+    public void pluschp(View v) {
+        chpcounter++;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("chpcounter", chpcounter);
+        editor.apply();
+        update();
+    }
+
+    public void minuschp(View v) {
+        chpcounter--;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("chpcounter", chpcounter);
+        editor.apply();
+        update();
+    }
+
+    // Current FP controls
+    public void pluscfp(View v) {
+        cfpcounter++;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("cfpcounter", cfpcounter);
+        editor.apply();
+        update();
+    }
+
+    public void minuscfp(View v) {
+        cfpcounter--;
+        SharedPreferences.Editor editor = saved.edit();
+        editor.putInt("cfpcounter", cfpcounter);
+        editor.apply();
+        update();
+    }
 
 }
