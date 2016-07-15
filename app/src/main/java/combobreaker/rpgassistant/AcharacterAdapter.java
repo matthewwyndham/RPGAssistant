1d5de23fd1fe624bee56a806ed66a62d0ef018ec
Jonathan <jester1570@gmail.com>
2016-07-15 01:30:11 -0600

    View button on character doesn't work

---
+++ app/src/main/java/combobreaker/rpgassistant/AcharacterAdapter.java
@@ -0,0 +1,77 @@
+package combobreaker.rpgassistant;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AcharacterAdapter extends BaseAdapter implements ListAdapter {
+    private ArrayList<Acharacter> list = new ArrayList<Acharacter>();
+    private Context context;
+
+    public AcharacterAdapter(ArrayList<Acharacter> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return 0; //list.get(pos).getId();
+        //just return 0 if your list items do not have an Id variable.
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.characterlistitem, null);
+        }
+
+        //Handle TextView and display string from your list
+        TextView listItemText = (TextView)view.findViewById(R.id.list_item_string);
+        listItemText.setText(list.get(position).nameofcharacter);
+
+        //Handle buttons and add onClickListeners
+        Button deleteBtn = (Button)view.findViewById(R.id.delete_btn);
+        Button viewBtn = (Button)view.findViewById(R.id.view_btn);
+
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+                list.remove(position); //or some other task
+                notifyDataSetChanged();
+            }
+        });
+        viewBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // todo: show the stats somehow...
+                Intent intent = new Intent(v.getContext(), CharacterStats.class);
+                intent.putParcelableArrayListExtra("characters", list);
+                intent.putExtra("which", position);
+                context.startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+}
